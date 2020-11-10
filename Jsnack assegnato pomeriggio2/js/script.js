b69e76c234dd60027eabbd2a21caf3a4ae9bdd7a
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-10 18:55:38 +0100

    ...

--- Jsnack assegnato pomeriggio2/js/script.js
+++ Jsnack assegnato pomeriggio2/js/script.js
@@ -21,24 +21,33 @@ var arrayObj = [
 
 console.log(arrayObj);
 
-var newArray = arrayObj;
 
-/* OPPURE FACENDO
+
+//devo creare una copia del primo array. se faccio il classico var newarray = oldarray, in console log vengono richiamati gli oggetti del primo console loge, quindi mi posizionerebbe la "posizione" anche nel primo arrayObj
+
+//devo dunque copiare si il primo array, ma nell'array copiato devo copiare singolarmente i contenuti rendendo il nuovo array effettivamente slegato dal primo
+
+//creo un nuovo array vuoto
 var newArray = [];
 
-for (var i of arrayObj) {
-  newArray.push(i);
+//faccio un ciclo lungo quanto l'array che voglio copiare
+for (var i = 0; i < arrayObj.length; i++) {
+
+  //Aggiungo ogni ciclo un nuovo oggetto con dentro la proprietà position al quale assegno una lettera casuale...
+
+  var letteraRandom = String.fromCharCode(97+Math.floor(Math.random() * 26));// al posto del 97 mettere 65 se si volgiono le lettere maiuscole
+  newArray.push({position: letteraRandom});
+
+  //infine copio le key dell'array vecchio dentro quello nuovo
+  for (var key in arrayObj[i]) {
+    newArray[i][key] = arrayObj[i][key];
+  }
 }
-*/
 
-
-
-
-for (var i = 0; i < newArray.length; i++) {
-  var numeroRandom = String.fromCharCode(97+Math.floor(Math.random() * 26));  // al posto del 97 mettere 65 se si volgiono le lettere maiuscole
-  newArray[i].position = numeroRandom;
-}
-
+//metto in ordine alfabetico le key nel nuovo array, ma si vedono in ordine alfabetico solo se apro i vari oggetti nel console.log(), se no restano con position primo, che è il primo che ho inserito nel nuovo array e poi gli altri che ho copiato ciclandoli dall'arrayvecchio
+newArray.sort(function(a, b) {
+  return a.distance - b.distance;
+});
 console.log(newArray);
 
 
