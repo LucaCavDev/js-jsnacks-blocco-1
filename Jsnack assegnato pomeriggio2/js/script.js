a1230e8d3398d1c5d4307b726e78d0be8217bb05
luca cavicchioli <luca.cavicchioli.88@gmail.com>
2020-11-10 17:41:15 +0100

    ...

--- Jsnack assegnato pomeriggio2/js/script.js
+++ Jsnack assegnato pomeriggio2/js/script.js
@@ -1,22 +1,44 @@
-var bombe = [];
-var numeroCasuale;
-var exit;
+//jsnack 2
+// dato questo array di obj iniziale:
+// var arrayObj = [
+//   {name: 'Poppy', type: 'tshirt', color: 'red'},
+//   {name: 'Jumping', type: 'occhiali', color: 'blue'},
+//   {name: 'CrissCross', type: 'scarpe', color: 'black'},
+//   {name: 'Jenny', type: 'borsa', color: 'pink'},
+// ];
+// //Creiamo una copia dell’array di partenza e aggiungiamo ai singoli elementi (quindi ogni oggetto del nuovo array)
+// una nuova proprietà position che contiene una lettera casuale. non dobbiamo modificare l’array iniziale
 
-for (var i = 0; i < 16; i++) {
-  // controllo se la bomba è già stata inserita
-  exit = false;
-  do {
-    numeroCasuale = randomNumber(100);
-    // console.log(numeroCasuale);
-    // se numero non presente
-    if (bombe.indexOf(numeroCasuale) === -1) {
-      bombe.push(numeroCasuale);
-      exit = true;
-    } else {
-      // console.log("numero già ripetuto: ", numeroCasuale);
-    }
-  } while (exit != true);
 
+
+
+var arrayObj = [
+  {name: 'Poppy', type: 'tshirt', color: 'red'},
+  {name: 'Jumping', type: 'occhiali', color: 'blue'},
+  {name: 'CrissCross', type: 'scarpe', color: 'black'},
+  {name: 'Jenny', type: 'borsa', color: 'pink'}
+];
+
+console.log(arrayObj);
+var newArray = [];
+
+for (var i of arrayObj) {
+  newArray.push(i);
 }
 
-console.log("bombe:", bombe);
+console.log(newArray);
+
+
+for (var i = 0; i < newArray.length; i++) {
+  var numeroRandom = String.fromCharCode(97+Math.floor(Math.random() * 26));  // al posto del 97 mettere 65 se si volgiono le lettere maiuscole
+  newArray[i].position = numeroRandom;
+}
+
+
+
+
+//funzioni generiche:
+//numeri random
+function getRandom(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
